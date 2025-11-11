b29edb25692600c9d58a8b5ec78944c3996a3e80
basalumutgazi <basalumutgazi@gmail.com>
2025-11-11 11:28:58 +0100

    changement du code de pour creer un sorti de html

--- miniprojet/programmes/miniprojet.sh
+++ miniprojet/programmes/miniprojet.sh
@@ -10,6 +10,29 @@ if [ -z "$1" ]; then
 	exit 1
 fi
 
+mkdir -p tableaux # pour créer une dossier s'il n'y en a pas déjà 
+
+fichier_sortie="../tableaux/tableau-fr.html"
+
+cat > "${fichier_sortie}" << EOF # EOF est utilisé pour écrire plusieurs lignes
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Tableau des URLs</title>
+</head>
+<body>
+    <h1>Analyse des URLs</h1>
+    <table border="1">
+        <tr>
+            <th>Numéro</th>
+            <th>URL</th>
+            <th>Code HTTP</th>
+            <th>Encodage</th>
+            <th>Nombre de mots</th>
+        </tr>
+EOF
+
 num=1
 while read -r ligne; do
 	code=$(curl -s -o /dev/null -w "%{http_code}" -L "$ligne")
@@ -23,6 +46,19 @@ while read -r ligne; do
 		n_mots="Pas de mots dans la page donnée"
 	fi
 
-	echo -e "${num}\t${ligne}\t${code}\t${encodage}\t${n_mots}" >> ../tableaux/tableau-fr.tsv
+	echo "		<tr>" >> "${fichier_sortie}"
+	echo "			<td>$num</td>" >> "${fichier_sortie}"
+	echo "			<td>$ligne</td>" >> "${fichier_sortie}"
+	echo "			<td>$code</td>" >> "${fichier_sortie}"
+	echo "			<td>$encodage</td>" >> "${fichier_sortie}"
+	echo "			<td>$n_mots</td>" >> "${fichier_sortie}"
+	echo "		</tr>" >> "${fichier_sortie}"
+
 	num=$((num+1))
 done < "$1";
+
+cat >> "${fichier_sortie}" << EOF
+	</table>
+</body>
+</html>
+EOF
